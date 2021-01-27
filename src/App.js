5adc5a0c4efaa1a67c459a7c9c19a0b400e69596
Dean Peter <deanpeter@switchmaven.com>
2021-01-26 21:42:43 -0800

    pop-up menu added

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import './App.css';
 import HelloWorld from "./components/helloworld"
 import OverView from "./components/overview"
 import Updatedable from "./components/updateable"
+import Skills from "./components/createSkills"
 function App() {
   return (
     <div className="App">
       <div className="Helloworld">
-       <HelloWorld></HelloWorld>
+        <HelloWorld></HelloWorld>
       </div>
     </div>
   );
--- src/components/helloworld.jsx
+++ src/components/helloworld.jsx
@@ -2,76 +2,147 @@ import React from 'react';
 import ReactFlow from 'react-flow-renderer';
 
 import "./app.css"
-const elements = [
-    {
-        id: '1',
-        type: 'input', // input node
-        data: { label: 'Input Node' },
-        position: { x: 250, y: 25 },
-    },
-    // default node
-    {
-        id: '2',
-        // you can also pass a React component as a label
-        data: { label: <div>
-            <div>
-                <button className="connect">+</button>
-            </div>
-        </div> },
-        position: { x: 100, y: 125 },
-    },
-    {
-        id: '3',
-        type: 'output', // output node
-        data: { label: < div >
-            <div>
-                <button className="disconnect">-</button>
-            </div>
-        </div > },
-        position: { x: 250, y: 250 },
-    },
-    // animated edge
-    { id: 'e1-2', source: '1', target: '2', animated: true },
-    { id: 'e2-3', source: '2', target: '3' },
-];
-
-//connection buttonn
-    const ConnectBtn =  ()=>{
-        return <div>
-                 <button> + </button>
-               </div>
-    }
-    //disconnect button
-    // const DisconnectBtn = () => {
-    //    alert("listerning",20000)
-    // }
-// const onElementClick = (event, element) => {
-//     alert(element.data.label,"is clicked")
-       
-//     };
-//     //create butoon
-    const CreateBtn = () => {
-        return <div>
-            <button> + </button>
-        </div>
-    }
-
-export default () => (
-    <div style={{ height: 1000 }}>
-        <ReactFlow elements={elements} >
-            
-        </ReactFlow>
-    </div>
-);
+import SkillsForm from "./createSkills"
+import Modal from "./Modal";
+import Form from "react-bootstrap/Form"
+// class components 
+class CreateSkills extends React.Component{
+    constructor(props){
+         super(props);
+         this.state = {
         
-// onElementClick = { onElementClick }
-// onElementsRemove = { onElementsRemove }
-// onConnect = { onConnect }
-// onNodeDragStop = { onNodeDragStop }
-// style = {{ background: bgColor }}
-// onLoad = { onLoad }
-// nodeTypes = { nodeTypes }
-// connectionLineStyle = { connectionLineStyle }
-// snapToGrid = { true}
-// snapGrid = { snapGrid }
-// defaultZoom = { 1.5} />
+             //state of the buttons
+             CreateButton : true,
+             connectButton : false,
+             disConnectButton : false,
+
+            //modal input
+             modal: false,
+             name: "",
+             modalInputName: "",
+             modalInputDiscription:""
+         }
+
+    }
+    handleChange(e) {
+        const target = e.target;
+        const name = target.name;
+        const value = target.value;
+
+        this.setState({
+            [name]: value
+        });
+    }
+
+    handleSubmit(e) {
+        this.setState({ name: this.state.modalInputName });
+        this.modalClose();
+    }
+
+    modalOpen() {
+        this.setState({ modal: true });
+    }
+
+    modalClose() {
+        this.setState({
+            modalInputName: "",
+            modalInputDiscription: "",
+            modal: false
+        });
+    }
+    render(){
+        // dummy nodes to the files
+        const elements = [
+            {
+                id: '1',
+                type: 'input', // input node
+                data: { label: 'Input Node' },
+                position: { x: 250, y: 25 },
+            },
+            // default node
+            {
+                id: '2',
+                // you can also pass a React component as a label
+                data: {
+                    label: <div>
+                        <div>
+                            <button
+                                onClick={e => this.modalOpen(e)}
+                                className="connect">+
+                        </button>
+                        </div>
+                    </div>
+                },
+                position: { x: 100, y: 125 },
+            },
+            {
+                id: '3',
+                type: 'output', // output node
+                data: {
+                    label: <div>
+                        <div>
+                            <button
+                                onClick={e => this.modalOpen(e)}
+                                className="connect">+
+                            </button>
+                        </div>
+                    </div>
+                },
+                position: { x: 250, y: 250 },
+            },
+            // animated edge
+            { id: 'e1-2', source: '1', target: '2', animated: true },
+            { id: 'e2-3', source: '2', target: '3' },
+        ];
+        return(
+            <div style={{height: 1000}} >
+                <ReactFlow elements={elements} >
+                </ReactFlow>
+
+                <div className="App">
+                    <Modal show={this.state.modal} handleClose={e => this.modalClose(e)}>
+                        {/* <div className="form-group">
+                            <label>Skills Name:</label>
+                            <input style ={{width:300, marginLeft:200}}
+                                type="text"
+                                value={this.state.modalInputName}
+                                name="modalInputName"
+                                onChange={e => this.handleChange(e)}
+                                className="form-control"
+                            />
+                        </div>
+                        <div>
+                        <label>Discription</label>
+                        <textarea
+                            type="text"
+                            value={this.state.modalInputDiscription}
+                            name="modalInputDiscripion"
+                            onChange={e => this.handleChange(e)}
+                            className="form-control"
+                        />
+                        </div>
+                        <div className="form-group">
+                            <button onClick={e => this.handleSubmit(e)} type="button">
+                                Save
+                        </button>
+                        </div> */}
+                        <Form>
+                            <Form.Group controlId="exampleForm.ControlInput1">
+                                <Form.Label>Name</Form.Label>
+                                <Form.Control type="email" placeholder="name@example.com" />
+                            </Form.Group>
+                            <Form.Group controlId="exampleForm.ControlTextarea1">
+                                <Form.Label>Description</Form.Label>
+                                <Form.Control as="textarea" rows={3} />
+                            </Form.Group>
+                        </Form>
+
+                    </Modal>
+                </div>
+            </div>
+        ) 
+    }
+}
+
+export default CreateSkills;
+        
